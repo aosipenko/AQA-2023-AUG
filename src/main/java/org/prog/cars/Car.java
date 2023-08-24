b532c654d35f16ccf04c4e230f0c49aed02bccdc
aosipenko <aoskiev@gmail.com>
2023-08-24 20:11:03 +0200

    Session 4: Streams

--- src/main/java/org/prog/cars/Car.java
+++ src/main/java/org/prog/cars/Car.java
@@ -1,69 +1,11 @@
 package org.prog.cars;
 
-import org.prog.Journey;
+public class Car {
 
-public class Car extends Vehicle implements ICar, IFuleable {
+    public Owner owner;
+    public String plateNumber;
 
+    //TODO: HOME WORK
     public String color;
-    public String carBrand;
 
-    public void goTo(Journey parameters) {
-        goTo(parameters.from, parameters.passingThrough, parameters.destination);
-    }
-
-    public void goTo(String destination) {
-        goTo("current position", destination);
-    }
-
-    public void goTo(String from, String destination) {
-        goTo(from, "no additional stops", destination);
-    }
-
-    public void goTo(String from, String passing, String destination) {
-        System.out.println("Car is going from " + from + " to "
-                + destination + " passing through " + passing);
-    }
-
-    public void fuelCar() {
-        System.out.println("Pour some gas");
-    }
-
-    @Override
-    public void moveForward(String distance) {
-        System.out.println("Car goes " + distance);
-    }
-
-    @Override
-    public void turnRight() {
-        System.out.println("Car turns Right");
-    }
-
-    @Override
-    public void turnLeft() {
-        System.out.println("Car turns Left");
-    }
-
-    @Override
-    public void stop() {
-        System.out.println("Car stops");
-    }
-
-    @Override
-    public void reverseMovement() {
-        System.out.println("Reverse car movement direction");
-    }
-
-    @Override
-    public void fuelVehicle() {
-        fuelCar();
-    }
-
-    @Override
-    public void setWheelSize() {
-        changeOil();
-    }
-
-    private void changeOil() {
-
-    }
 }
--- src/main/java/org/prog/cars/Truck.java
+++
@@ -1,9 +0,0 @@
-package org.prog.cars;
-
-public class Truck extends Car {
-
-    public void putCargo(){
-        smth();
-    }
-
-}
